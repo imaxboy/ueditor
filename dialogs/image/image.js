bea8a6faf3006647fa78d1a8ea1e0fb417ff1735
maxboy <496604841@qq.com>
2018-03-19 16:43:01 +0800

    Update image dialog

--- dialogs/image/image.js
+++ dialogs/image/image.js
@@ -7,14 +7,10 @@
 
 (function () {
 
-    var remoteImage,
-        uploadImage,
-        onlineImage,
-        searchImage;
+    var remoteImage, uploadImage;
 
     window.onload = function () {
         initTabs();
-        initAlign();
         initButtons();
     };
 
@@ -55,17 +51,7 @@
                 remoteImage = remoteImage || new RemoteImage();
                 break;
             case 'upload':
-                setAlign(editor.getOpt('imageInsertAlign'));
                 uploadImage = uploadImage || new UploadImage('queueList');
-                break;
-            case 'online':
-                setAlign(editor.getOpt('imageManagerInsertAlign'));
-                onlineImage = onlineImage || new OnlineImage('imageList');
-                onlineImage.reset();
-                break;
-            case 'search':
-                setAlign(editor.getOpt('imageManagerInsertAlign'));
-                searchImage = searchImage || new SearchImage();
                 break;
         }
     }
@@ -94,13 +80,6 @@
                         return false;
                     }
                     break;
-                case 'online':
-                    list = onlineImage.getInsertList();
-                    break;
-                case 'search':
-                    list = searchImage.getInsertList();
-                    remote = true;
-                    break;
             }
 
             if(list) {
@@ -109,38 +88,6 @@
             }
         };
     }
-
-
-    /* 初始化对其方式的点击事件 */
-    function initAlign(){
-        /* 点击align图标 */
-        domUtils.on($G("alignIcon"), 'click', function(e){
-            var target = e.target || e.srcElement;
-            if(target.className && target.className.indexOf('-align') != -1) {
-                setAlign(target.getAttribute('data-align'));
-            }
-        });
-    }
-
-    /* 设置对齐方式 */
-    function setAlign(align){
-        align = align || 'none';
-        var aligns = $G("alignIcon").children;
-        for(i = 0; i < aligns.length; i++){
-            if(aligns[i].getAttribute('data-align') == align) {
-                domUtils.addClass(aligns[i], 'focus');
-                $G("align").value = aligns[i].getAttribute('data-align');
-            } else {
-                domUtils.removeClasses(aligns[i], 'focus');
-            }
-        }
-    }
-    /* 获取对齐方式 */
-    function getAlign(){
-        var align = $G("align").value || 'none';
-        return align == 'none' ? '':align;
-    }
-
 
     /* 在线图片 */
     function RemoteImage(target) {
@@ -160,7 +107,6 @@
                 'border': $G('border'),
                 'vhSpace': $G('vhSpace'),
                 'title': $G('title'),
-                'align': $G('align')
             };
             var img = editor.selection.getRange().getClosedNode();
             if (img) {
@@ -221,8 +167,7 @@
             if (!img.tagName || img.tagName.toLowerCase() != 'img' && !img.getAttribute("src") || !img.src) return;
 
             var wordImgFlag = img.getAttribute("word_img"),
-                src = wordImgFlag ? wordImgFlag.replace("&amp;", "&") : (img.getAttribute('_src') || img.getAttribute("src", 2).replace("&amp;", "&")),
-                align = editor.queryCommandValue("imageFloat");
+                src = wordImgFlag ? wordImgFlag.replace("&amp;", "&") : (img.getAttribute('_src') || img.getAttribute("src", 2).replace("&amp;", "&"));
 
             /* 防止onchange事件循环调用 */
             if (src !== $G("url").value) $G("url").value = src;
@@ -233,7 +178,6 @@
                 $G("border").value = img.getAttribute("border") || '0';
                 $G("vhSpace").value = img.getAttribute("vspace") || '0';
                 $G("title").value = img.title || img.alt || '';
-                setAlign(align);
                 this.setPreview();
                 this.updateLocker();
             }
@@ -272,7 +216,6 @@
                     width: data['width'] || '',
                     height: data['height'] || '',
                     border: data['border'] || '',
-                    floatStyle: data['align'] || '',
                     vspace: data['vhSpace'] || '',
                     alt: data['title'] || '',
                     style: "width:" + data['width'] + "px;height:" + data['height'] + "px;"
@@ -282,8 +225,6 @@
             }
         }
     };
-
-
 
     /* 上传图片 */
     function UploadImage(target) {
@@ -377,7 +318,7 @@
                     width: imageCompressBorder,
                     height: imageCompressBorder,
                     // 图片质量，只有type为`image/jpeg`的时候才有效。
-                    quality: 90,
+                    quality: 100,
                     // 是否允许放大，如果想要生成小图的时候不失真，此选项应该设置为false.
                     allowMagnify: false,
                     // 是否允许裁剪。
@@ -771,7 +712,6 @@
         },
         getInsertList: function () {
             var i, data, list = [],
-                align = getAlign(),
                 prefix = editor.getOpt('imageUrlPrefix');
             for (i = 0; i < this.imageList.length; i++) {
                 data = this.imageList[i];
@@ -779,345 +719,9 @@
                     src: prefix + data.url,
                     _src: prefix + data.url,
                     alt: data.original,
-                    floatStyle: align
                 });
             }
             return list;
         }
     };
-
-
-    /* 在线图片 */
-    function OnlineImage(target) {
-        this.container = utils.isString(target) ? document.getElementById(target) : target;
-        this.init();
-    }
-    OnlineImage.prototype = {
-        init: function () {
-            this.reset();
-            this.initEvents();
-        },
-        /* 初始化容器 */
-        initContainer: function () {
-            this.container.innerHTML = '';
-            this.list = document.createElement('ul');
-            this.clearFloat = document.createElement('li');
-
-            domUtils.addClass(this.list, 'list');
-            domUtils.addClass(this.clearFloat, 'clearFloat');
-
-            this.list.appendChild(this.clearFloat);
-            this.container.appendChild(this.list);
-        },
-        /* 初始化滚动事件,滚动到地步自动拉取数据 */
-        initEvents: function () {
-            var _this = this;
-
-            /* 滚动拉取图片 */
-            domUtils.on($G('imageList'), 'scroll', function(e){
-                var panel = this;
-                if (panel.scrollHeight - (panel.offsetHeight + panel.scrollTop) < 10) {
-                    _this.getImageData();
-                }
-            });
-            /* 选中图片 */
-            domUtils.on(this.container, 'click', function (e) {
-                var target = e.target || e.srcElement,
-                    li = target.parentNode;
-
-                if (li.tagName.toLowerCase() == 'li') {
-                    if (domUtils.hasClass(li, 'selected')) {
-                        domUtils.removeClasses(li, 'selected');
-                    } else {
-                        domUtils.addClass(li, 'selected');
-                    }
-                }
-            });
-        },
-        /* 初始化第一次的数据 */
-        initData: function () {
-
-            /* 拉取数据需要使用的值 */
-            this.state = 0;
-            this.listSize = editor.getOpt('imageManagerListSize');
-            this.listIndex = 0;
-            this.listEnd = false;
-
-            /* 第一次拉取数据 */
-            this.getImageData();
-        },
-        /* 重置界面 */
-        reset: function() {
-            this.initContainer();
-            this.initData();
-        },
-        /* 向后台拉取图片列表数据 */
-        getImageData: function () {
-            var _this = this;
-
-            if(!_this.listEnd && !this.isLoadingData) {
-                this.isLoadingData = true;
-                var url = editor.getActionUrl(editor.getOpt('imageManagerActionName')),
-                    isJsonp = utils.isCrossDomainUrl(url);
-                ajax.request(url, {
-                    'timeout': 100000,
-                    'dataType': isJsonp ? 'jsonp':'',
-                    'data': utils.extend({
-                        start: this.listIndex,
-                        size: this.listSize
-                    }, editor.queryCommandValue('serverparam')),
-                    'method': 'get',
-                    'onsuccess': function (r) {
-                        try {
-                            var json = isJsonp ? r:eval('(' + r.responseText + ')');
-                            if (json.state == 'SUCCESS') {
-                                _this.pushData(json.list);
-                                _this.listIndex = parseInt(json.start) + parseInt(json.list.length);
-                                if(_this.listIndex >= json.total) {
-                                    _this.listEnd = true;
-                                }
-                                _this.isLoadingData = false;
-                            }
-                        } catch (e) {
-                            if(r.responseText.indexOf('ue_separate_ue') != -1) {
-                                var list = r.responseText.split(r.responseText);
-                                _this.pushData(list);
-                                _this.listIndex = parseInt(list.length);
-                                _this.listEnd = true;
-                                _this.isLoadingData = false;
-                            }
-                        }
-                    },
-                    'onerror': function () {
-                        _this.isLoadingData = false;
-                    }
-                });
-            }
-        },
-        /* 添加图片到列表界面上 */
-        pushData: function (list) {
-            var i, item, img, icon, _this = this,
-                urlPrefix = editor.getOpt('imageManagerUrlPrefix');
-            for (i = 0; i < list.length; i++) {
-                if(list[i] && list[i].url) {
-                    item = document.createElement('li');
-                    img = document.createElement('img');
-                    icon = document.createElement('span');
-
-                    domUtils.on(img, 'load', (function(image){
-                        return function(){
-                            _this.scale(image, image.parentNode.offsetWidth, image.parentNode.offsetHeight);
-                        }
-                    })(img));
-                    img.width = 113;
-                    img.setAttribute('src', urlPrefix + list[i].url + (list[i].url.indexOf('?') == -1 ? '?noCache=':'&noCache=') + (+new Date()).toString(36) );
-                    img.setAttribute('_src', urlPrefix + list[i].url);
-                    domUtils.addClass(icon, 'icon');
-
-                    item.appendChild(img);
-                    item.appendChild(icon);
-                    this.list.insertBefore(item, this.clearFloat);
-                }
-            }
-        },
-        /* 改变图片大小 */
-        scale: function (img, w, h, type) {
-            var ow = img.width,
-                oh = img.height;
-
-            if (type == 'justify') {
-                if (ow >= oh) {
-                    img.width = w;
-                    img.height = h * oh / ow;
-                    img.style.marginLeft = '-' + parseInt((img.width - w) / 2) + 'px';
-                } else {
-                    img.width = w * ow / oh;
-                    img.height = h;
-                    img.style.marginTop = '-' + parseInt((img.height - h) / 2) + 'px';
-                }
-            } else {
-                if (ow >= oh) {
-                    img.width = w * ow / oh;
-                    img.height = h;
-                    img.style.marginLeft = '-' + parseInt((img.width - w) / 2) + 'px';
-                } else {
-                    img.width = w;
-                    img.height = h * oh / ow;
-                    img.style.marginTop = '-' + parseInt((img.height - h) / 2) + 'px';
-                }
-            }
-        },
-        getInsertList: function () {
-            var i, lis = this.list.children, list = [], align = getAlign();
-            for (i = 0; i < lis.length; i++) {
-                if (domUtils.hasClass(lis[i], 'selected')) {
-                    var img = lis[i].firstChild,
-                        src = img.getAttribute('_src');
-                    list.push({
-                        src: src,
-                        _src: src,
-                        alt: src.substr(src.lastIndexOf('/') + 1),
-                        floatStyle: align
-                    });
-                }
-
-            }
-            return list;
-        }
-    };
-
-    /*搜索图片 */
-    function SearchImage() {
-        this.init();
-    }
-    SearchImage.prototype = {
-        init: function () {
-            this.initEvents();
-        },
-        initEvents: function(){
-            var _this = this;
-
-            /* 点击搜索按钮 */
-            domUtils.on($G('searchBtn'), 'click', function(){
-                var key = $G('searchTxt').value;
-                if(key && key != lang.searchRemind) {
-                    _this.getImageData();
-                }
-            });
-            /* 点击清除妞 */
-            domUtils.on($G('searchReset'), 'click', function(){
-                $G('searchTxt').value = lang.searchRemind;
-                $G('searchListUl').innerHTML = '';
-                $G('searchType').selectedIndex = 0;
-            });
-            /* 搜索框聚焦 */
-            domUtils.on($G('searchTxt'), 'focus', function(){
-                var key = $G('searchTxt').value;
-                if(key && key == lang.searchRemind) {
-                    $G('searchTxt').value = '';
-                }
-            });
-            /* 搜索框回车键搜索 */
-            domUtils.on($G('searchTxt'), 'keydown', function(e){
-                var keyCode = e.keyCode || e.which;
-                if (keyCode == 13) {
-                    $G('searchBtn').click();
-                }
-            });
-
-            /* 选中图片 */
-            domUtils.on($G('searchList'), 'click', function(e){
-                var target = e.target || e.srcElement,
-                    li = target.parentNode.parentNode;
-
-                if (li.tagName.toLowerCase() == 'li') {
-                    if (domUtils.hasClass(li, 'selected')) {
-                        domUtils.removeClasses(li, 'selected');
-                    } else {
-                        domUtils.addClass(li, 'selected');
-                    }
-                }
-            });
-        },
-        /* 改变图片大小 */
-        scale: function (img, w, h) {
-            var ow = img.width,
-                oh = img.height;
-
-            if (ow >= oh) {
-                img.width = w * ow / oh;
-                img.height = h;
-                img.style.marginLeft = '-' + parseInt((img.width - w) / 2) + 'px';
-            } else {
-                img.width = w;
-                img.height = h * oh / ow;
-                img.style.marginTop = '-' + parseInt((img.height - h) / 2) + 'px';
-            }
-        },
-        getImageData: function(){
-            var _this = this,
-                key = $G('searchTxt').value,
-                type = $G('searchType').value,
-                keepOriginName = editor.options.keepOriginName ? "1" : "0",
-                url = "http://image.baidu.com/i?ct=201326592&cl=2&lm=-1&st=-1&tn=baiduimagejson&istype=2&rn=32&fm=index&pv=&word=" + key + type + "&ie=utf-8&oe=utf-8&keeporiginname=" + keepOriginName + "&" + +new Date;
-
-            $G('searchListUl').innerHTML = lang.searchLoading;
-            ajax.request(url, {
-                'dataType': 'jsonp',
-                'charset': 'GB18030',
-                'onsuccess':function(json){
-                    var list = [];
-                    if(json && json.data) {
-                        for(var i = 0; i < json.data.length; i++) {
-                            if(json.data[i].objURL) {
-                                list.push({
-                                    title: json.data[i].fromPageTitleEnc,
-                                    src: json.data[i].objURL,
-                                    url: json.data[i].fromURL
-                                });
-                            }
-                        }
-                    }
-                    _this.setList(list);
-                },
-                'onerror':function(){
-                    $G('searchListUl').innerHTML = lang.searchRetry;
-                }
-            });
-        },
-        /* 添加图片到列表界面上 */
-        setList: function (list) {
-            var i, item, p, img, link, _this = this,
-                listUl = $G('searchListUl');
-
-            listUl.innerHTML = '';
-            if(list.length) {
-                for (i = 0; i < list.length; i++) {
-                    item = document.createElement('li');
-                    p = document.createElement('p');
-                    img = document.createElement('img');
-                    link = document.createElement('a');
-
-                    img.onload = function () {
-                        _this.scale(this, 113, 113);
-                    };
-                    img.width = 113;
-                    img.setAttribute('src', list[i].src);
-
-                    link.href = list[i].url;
-                    link.target = '_blank';
-                    link.title = list[i].title;
-                    link.innerHTML = list[i].title;
-
-                    p.appendChild(img);
-                    item.appendChild(p);
-                    item.appendChild(link);
-                    listUl.appendChild(item);
-                }
-            } else {
-                listUl.innerHTML = lang.searchRetry;
-            }
-        },
-        getInsertList: function () {
-            var child,
-                src,
-                align = getAlign(),
-                list = [],
-                items = $G('searchListUl').children;
-            for(var i = 0; i < items.length; i++) {
-                child = items[i].firstChild && items[i].firstChild.firstChild;
-                if(child.tagName && child.tagName.toLowerCase() == 'img' && domUtils.hasClass(items[i], 'selected')) {
-                    src = child.src;
-                    list.push({
-                        src: src,
-                        _src: src,
-                        alt: src.substr(src.lastIndexOf('/') + 1),
-                        floatStyle: align
-                    });
-                }
-            }
-            return list;
-        }
-    };
-
 })();
